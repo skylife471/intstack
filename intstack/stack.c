714b49dd85f5e77914729971a2366a3bf0ba1748
skyli <skyli@DESKTOP-12RT62E>
2023-03-29 15:49:31 +0900

    초기 커밋.

---
+++ intstack/stack.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define MAX_SIZE 10
+typedef int element;
+element stack[MAX_SIZE];
+int top = -1;
+
+int is_empty()
+{
+	return (top == -1);
+}
+int is_full()
+{
+	return (top == (MAX_SIZE - 1));
+}
+void push(element item)
+{
+	if (is_full)
+	{
+		fprintf(stderr, "Stack Full!\n");
+	}
+	else
+	{
+		stack[++top] = item;
+	}
+}
+element pop()
+{
+	if (is_empty)
+	{
+		fprintf(stderr, "Stack Empty\n");
+		return;
+	}
+	else
+		return stack[top--];
+}
+int main()
+{
+	int num;
+	srand(time(NULL));
+	int rand_num= rand() % 100 + 1;
+	for (int i = 0;i <30; i++)
+	{
+		if (rand_num % 2 == 0)
+		{
+			push(rand_num);
+			printf("[%d} Push %d\n", i + 1, rand_num);
+		}
+		if (rand_num % 2 == 1)
+		{
+			printf("[%d] Pop %d\n",i+1, pop());
+		}
+		rand_num = rand() % 100 + 1;
+	}
+}
